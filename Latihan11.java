c41f50c4e0ea55cefda6f08c8eb3eead96b5314c
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2022-09-26 22:34:26 +0700

    Initial commit

---
+++ Latihan11.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nella230922;
+
+/**
+ *
+ * @author Asus
+ */
+public class Latihan11 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 45;
+        float avg =(number1 + number2 + number3) /3;
+        
+        System.out.println("number 1 = " + number1);
+        System.out.println("number 2 = " + number2);
+        System.out.println("number 3 = " + number3);
+        System.out.println("average is = " + avg);
+    }
+}
---
+++ Latihan12.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nella230922;
+
+/**
+ *
+ * @author Asus
+ */
+public class Latihan12 {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 23;
+        int number3 = 5;
+        
+        long nilai = (number1>number2)?number1:(number2>number3)?number2:(number3>number1)?number3:number1; 
+        
+        System.out.println("number 1 = "+ number1);
+        System.out.println("number 2 = "+ number2);
+        System.out.println("number 3 = "+ number3);
+        System.out.println("Nilai tertingginya adalah angka = "+nilai);
+    }
+    
+    
+}
